71fd7130daec9af724be0053bc8bfb17d419f5f7
Ivan Tsybulin <tsybulinhome@gmail.com>
2021-06-23 10:14:14 +0300

    Initial version

---
+++ app.py
@@ -0,0 +1,79 @@
+import logging
+import re
+import time
+
+from flask import Flask, request
+from flask.json import jsonify
+
+from sender import SendQueue, SenderThread
+
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)5s %(message)s')
+logger = logging.getLogger(__name__)
+
+app = Flask(__name__)
+send_queue = SendQueue(maxsize=300)
+SenderThread(send_queue).start()
+
+filters = {}
+filter_id = 0
+
+def to_model(filter):
+    return {
+        'id': filter['id'],
+        'pattern': filter['pattern'],
+    }
+
+@app.route("/filters", methods=['GET'])
+def list_filters():
+    return jsonify([to_model(filter) for filter in filters.values()])
+
+@app.route("/filters", methods=['POST'])
+def add_filter():
+    if not request.json or not 'pattern' in request.json:
+        return jsonify({'message': 'field "pattern" is missing from json request'}), 400
+    pattern = request.json['pattern']
+    try:
+        compiled = re.compile(pattern)
+    except Exception as e:
+        return jsonify({'message': str(e)}), 400
+
+    logger.info('Adding filter with pattern %s', pattern)
+    global filter_id
+    filter_id += 1
+    filters[filter_id] = {
+        'id': filter_id,
+        'pattern': pattern,
+        'compiled': compiled
+    }
+    return jsonify(to_model(filters[filter_id]))
+
+@app.route("/filters/<int:filter_id>", methods=['GET'])
+def get_filter(filter_id):
+    filter = filters.get(filter_id)
+    if filter is None:
+        return jsonify({'message': 'no filter with id=' + str(filter_id)}), 404
+    return jsonify(to_model(filters[filter_id]))
+
+@app.route("/filters/<int:filter_id>", methods=['DELETE'])
+def del_filter(filter_id):
+    if filter_id not in filters:
+        return jsonify({'message': 'no filter with id=' + str(filter_id)}), 404
+    filter = filters.get(filter_id)
+    logger.info('Removing filter with pattern %s', filter['pattern'])
+    del filters[filter_id]
+    return jsonify(to_model(filter)), 204
+
+@app.route("/send", methods=["POST"])
+def send():
+    if not request.json or not 'body' in request.json:
+        return jsonify({'message': 'field "body" is missing from json request'}), 400
+    body = request.json['body']
+    for filter in filters.values():
+        id = filter['id']
+        compiled = filter['compiled']
+        if compiled.matches() is not None:
+            return jsonify({'message': 'forbidden by filter id=' + str(id)}), 400
+    if not send_queue.accept(time.time(), body):
+        return jsonify({'message': 'the queue is full'}), 429
+    return jsonify({}), 202
---
+++ sender.py
@@ -0,0 +1,108 @@
+import logging
+import time
+import threading
+import random
+
+from queue import PriorityQueue, Full
+
+logger = logging.getLogger(__name__)
+
+
+class Message:
+    def __init__(self, body, send_at, queued_at, attempt=0):
+        self.body = body
+        self.send_at = send_at
+        self.queued_at = queued_at
+        self.attempt = attempt
+
+    def make_next_attempt(self, delay=5):
+        send_at = time.time() + delay  # schedule send at now + delay sec
+        return Message(self.body, send_at, self.queued_at, self.attempt + 1)
+
+    def __lt__(self, other):
+        return self.send_at < other.send_at
+
+    def __repr__(self):
+        body = self.body
+        body = body[:17] + '...' if len(body) > 20 else body
+        delta = self.send_at - time.time()
+        return 'Message{%s, attempt=%d, send_at=now%+gs}' % (body, self.attempt, delta)
+
+
+class SendQueue(PriorityQueue):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def accept(self, now, body):
+        message = Message(body, now, now)
+        try:
+            super().put_nowait(message)
+        except Full:
+            return False
+        return True
+
+
+# https://en.wikipedia.org/wiki/Token_bucket
+class RateLimiter:
+    def __init__(self, bucket_size, window_secs):
+        self.bucket_size = bucket_size
+        self.last_tick = time.time()
+        self.available = bucket_size
+        self.window_secs = window_secs
+
+    def is_allowed(self):
+        now = time.time()
+        passed = now - self.last_tick
+        self.last_tick = now
+        self.available += passed * self.bucket_size / self.window_secs
+        if self.available > self.bucket_size:
+            self.available = self.bucket_size
+        if self.available < 1:
+            return False
+        self.available -= 1
+        return True
+
+
+class SenderThread(threading.Thread):
+    def __init__(self, queue):
+        super().__init__(name="Sender")
+        self.setDaemon(True)
+        self.queue = queue
+        self.rate_limiter = RateLimiter(10, 5)  # 10 msg per 5s
+
+    def run(self):
+        while True:
+            while True:
+                item = self.queue.get()
+                now = time.time()
+                logger.debug('Dequeued %s at %s', item, now)
+                if item.send_at > now:
+                    logger.debug('Too early, enqueued %s back', item)
+                    self.queue.put(item)
+                    break
+                self.try_send(item)
+
+            time.sleep(0.1)
+
+    def try_send(self, item):
+        logger.debug('Trying to send %s', item)
+        try:
+            if self.send(item):
+                logger.info('Sent %s', item)
+            else:
+                # Send failed, schedule retry
+                item = item.make_next_attempt()
+                self.queue.put(item)
+                logging.info('Postponed message %s', item)
+        except Exception as e:
+            logger.error('Exception in try_send', e)
+
+    def send(self, item):
+        # /dev/null is a proper place for spam
+        if not self.rate_limiter.is_allowed():
+            return False
+        lag = time.time() - item.queued_at
+        logger.debug('Sending %s with lag %gs', item, lag)
+        send_time = 0.2 + 0.3 * random.random()
+        time.sleep(send_time)
+        return True
